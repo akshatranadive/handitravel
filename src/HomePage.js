06ee8fc77c9e2814a908234b739c5f8fccfda9ea
akshat1920 <60009548+akshatranadive@users.noreply.github.com>
2023-03-20 11:27:05 -0400

    css changes

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,5 @@
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import './App.css'; // Import custom styles for the App component
+import './HomePage.css'; // Import custom styles for the App component
 import hotels from './assets/hotels.jpg';
 import commute from './assets/commute.jpg';
 // import logoup from './assets/logoup.png';
@@ -19,6 +19,7 @@ const Homepage = () =>{
 
 
   return (
+    <div className='bimage'>
     <div className="container">
       {/* <div className="bg-image bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white"  src={happy}>
         asffsdfwf
@@ -31,9 +32,10 @@ const Homepage = () =>{
       </div> */}
          {/* Background imaage of happy disabled people */}
       
+      
       <div className='mx-3'>
         <div className="img">
-       
+       <br></br>
           {/* <h1>Welcome to Handi Travel!!</h1> */}
           <h1>{t("greeting")}</h1>
           <p>{t("We are dedicated")}</p>
@@ -49,7 +51,7 @@ const Homepage = () =>{
                 <Card style={{ width: '20rem', height: '10rem' }}>
                   <Card.Img variant="top" style={{ height: '10rem' }} src={hotels} />
                   <Card.Body>
-                    <Card.Title>{t("Tour")}</Card.Title>
+                  <Card.Title>{t("Tour")}</Card.Title>
                   </Card.Body>
                 </Card>
               </a>
@@ -62,38 +64,44 @@ const Homepage = () =>{
         </Container>
         
         </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
         <div className='my-5'>
         {/* <h2>Our Features</h2> */}
         <h2>{t("our features")}</h2>
           <ul>
             {/* <li>Easy-to-use interface</li> */}
-            <li>{t("easy")}</li>
+            <p>{t("easy")}</p>
             {/*<li>Search and compare prices for hotels, flights, and buses</li> */}
-            <li>{t("search")}</li>
+            <p>{t("srch")}</p>
             {/*<li>Customizable options for accessibility needs</li> */}
-            <li>{t("options")}</li>
+            <p>{t("options")}</p>
           </ul>
-          <br />
+          {/* <br /> */}
           {/*<h2>Booking for Disabled</h2> */}
-          <h2>{t("booking")}</h2>
+          {/* <h2>{t("booking")}</h2> */}
           {/*<p>We understand the unique challenges that people with disabilities face when traveling, and we strive to make the process as smooth and stress-free as possible. Our website offers a variety of features specifically tailored to the needs of people with disabilities, including:</p> */}
-          <p>{t("understand")}</p>
-          <ul>
+          {/* <p>{t("understand")}</p> */}
+          {/* <ul> */}
             {/*<li>Wheelchair accessibility options for hotels, flights, and buses</li> */}
-            <li>{t("wheelchair")}</li>
+            {/* <li>{t("wheelchair")}</li> */}
             {/*<li>Assistance for hearing or visually impaired travelers</li> */}
-            <li>{t("assistance")}</li>
+            {/* <li>{t("assistance")}</li> */}
             {/*<li>Special needs assistance, such as service animals and mobility equipment rental</li> */}
-            <li>{t("special")}</li>
-          </ul>
+            {/* <li>{t("special")}</li> */}
+          {/* </ul> */}
   </div>
 
 
-  <div>
+  {/* <div> */}
   {/*<h2>Price Comparison</h2> */}
-  <h2>{t("price")}</h2>
+  {/* <h2>{t("price")}</h2> */}
   {/*<p>We are committed to providing our users with the best prices for their travel needs. Our website allows you to easily compare prices from different providers, so you can be sure you're getting the best deal. </p> */}
-  <p>{t("commited")}</p>
+  {/* <p>{t("commited")}</p> */}
+  {/* </div> */}
   </div>
   </div>
   </div>)}
